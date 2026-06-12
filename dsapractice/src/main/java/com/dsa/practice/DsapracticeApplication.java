ea4282d968b9dd908465c515b74336e484014703
SriRepo-cloud <srikanthbattini96@gmail.com>
2026-06-12 21:35:51 +0530

    created DSAPractice project

---
+++ dsapractice/src/main/java/com/dsa/practice/DsapracticeApplication.java
@@ -0,0 +1,14 @@
+package com.dsa.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsapracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsapracticeApplication.class, args);
+		System.out.println("Spring boot app started....");
+	}
+
+}
